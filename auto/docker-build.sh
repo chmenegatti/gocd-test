bbb24836dd9385290063a29c8e7a1d66c86178bb
Cesar Menegatti <csrmenegatti@gmail.com>
2023-02-24 08:30:16 -0300

    Changed Version Variable

--- auto/docker-build.sh
+++ auto/docker-build.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 #build image
-docker build -t chmenegatti/gocd-test:v$VERSION ../.
+docker build -t chmenegatti/gocd-test:v$GO_PIPELINE_COUNTER ../.
 
 #login into docker-hub
 echo $DOCKER_PASS >> pass.txt
 cat pass.txt | docker login -u chmenegatti --password-stdin
 
 #push image to docker hub
-docker push chmenegatti/gocd-test:v$VERSION
+docker push chmenegatti/gocd-test:v$GO_PIPELINE_COUNTER
 docker push chmenegatti/gocd-test:latest
