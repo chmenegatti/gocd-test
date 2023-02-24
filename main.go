2e9d528ddc4c7af8aedfbd94a35126a929ce5134
Cesar Menegatti <csrmenegatti@gmail.com>
2023-02-23 21:42:53 -0300

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
+
+		return c.SendString("<h1>Hello, World!</h1>")
+	})
+
+	app.Listen(":8080")
+}
